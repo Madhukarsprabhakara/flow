9a8fd133a67511f370ac2a6044dda0bcb630a8ff
Madhukar Prabhakara <madhu@Madhukars-MacBook-Air.local>
2022-03-09 14:36:40 -0800

    first commit

---
+++ database/migrations/2022_03_09_223002_create_connectors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('connectors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('type');
+            $table->text('description')->nullable();
+            $table->text('logo_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('connectors');
+    }
+};
